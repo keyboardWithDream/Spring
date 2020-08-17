b63e7f5ad1ba1c9a475fbeab8a8b56a23995e69e
keyboardWithDream <isharlan.hu@gmail.com>
2020-08-17 14:14:41 +0800

    基于Annotation的IoC案例

---
+++ L8_IoC_Anno_Case/src/main/java/com/study/spring/domian/Account.java
@@ -0,0 +1,47 @@
+package com.study.spring.domian;
+
+import java.io.Serializable;
+
+/**
+ * @author Harlan
+ * @date 2020/8/16 18:11
+ */
+public class Account implements Serializable {
+
+    private Integer id;
+    private String name;
+    private Double money;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getMoney() {
+        return money;
+    }
+
+    public void setMoney(Double money) {
+        this.money = money;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", money=" + money +
+                '}';
+    }
+}
